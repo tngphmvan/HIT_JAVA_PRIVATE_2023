1de4fbd9ef486739bd9c77e1d4271768d2635c37
Tung29022004@ <tungpv2902@gmail.com>
2023-10-23 18:30:47 +0700

    Bai_1

---
+++ btvn_buoi1_java_private_2023/src/main/java/com/company/btvn_buoi1_java_private_2023/Btvn_buoi1_java_private_2023.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.company.btvn_buoi1_java_private_2023;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 84393
+ */
+public class Btvn_buoi1_java_private_2023 {
+
+    public static void main(String[] args) {
+        int a = 0, b = 0, c = 0, gcd_temp = 0, temp = 0, gcd = 0;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        int tmp_a = a, tmp_b = b, tmp_c = c, lcm_tmp, lcm = 0, k = a * b * c;
+        while (b != 0) {
+            temp = a % b;
+            a = b;
+            b = temp;
+            gcd_temp = a;
+        }
+        while (c != 0) {
+            temp = gcd_temp % c;
+            gcd_temp = c;
+            c = temp;
+            gcd = gcd_temp;
+        }
+        System.out.println(gcd);
+        lcm_tmp = (tmp_a * tmp_b) / a;
+        int tmp_c1 = tmp_c;
+        int lcm_tmp1 = lcm_tmp;
+        while (tmp_c1 != 0) {
+            temp = lcm_tmp1 % tmp_c1;
+            lcm_tmp1 = tmp_c1;
+            tmp_c1 = temp;
+            gcd = lcm_tmp1;
+        }
+        System.out.println(k / gcd);
+    }
+}
