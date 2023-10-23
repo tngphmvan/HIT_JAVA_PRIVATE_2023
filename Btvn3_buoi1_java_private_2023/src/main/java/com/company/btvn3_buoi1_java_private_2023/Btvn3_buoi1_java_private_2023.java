eb7575bba1f2500fb92c954066153e1d271873e4
Tung29022004@ <tungpv2902@gmail.com>
2023-10-23 20:09:27 +0700

    Bai_3

--- Btvn3_buoi1_java_private_2023/src/main/java/com/company/btvn3_buoi1_java_private_2023/Btvn3_buoi1_java_private_2023.java
+++ Btvn3_buoi1_java_private_2023/src/main/java/com/company/btvn3_buoi1_java_private_2023/Btvn3_buoi1_java_private_2023.java
@@ -4,6 +4,8 @@
 
 package com.company.btvn3_buoi1_java_private_2023;
 
+import java.util.Scanner;
+
 /**
  *
  * @author 84393
@@ -11,6 +13,15 @@ package com.company.btvn3_buoi1_java_private_2023;
 public class Btvn3_buoi1_java_private_2023 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int a, tmp = 0, result = 1;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+
+        while (a > 0) {
+            tmp = a % 10; // 
+            result *= tmp; 
+            a /= 10; 
     }
+        System.out.println(result);
+}
 }
