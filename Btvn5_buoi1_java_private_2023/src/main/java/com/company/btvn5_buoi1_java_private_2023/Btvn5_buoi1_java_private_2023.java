acf36120a7c8a4920cfa6977e3803059100a73b9
Tung29022004@ <tungpv2902@gmail.com>
2023-10-23 23:00:37 +0700

    Bai_1

---
+++ Btvn5_buoi1_java_private_2023/src/main/java/com/company/btvn5_buoi1_java_private_2023/Btvn5_buoi1_java_private_2023.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.btvn5_buoi1_java_private_2023;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 84393
+ */
+public class Btvn5_buoi1_java_private_2023 {
+
+    public static void main(String[] args) {
+        int a, b,c,d,e,tmp1 = 0,tmp2,tmp3,tmp4,tmp5;
+        Scanner s = new Scanner(System.in);
+        a = s.nextInt();
+        b = s.nextInt();
+        c = s.nextInt();
+        d = s.nextInt();
+        e = s.nextInt();
+        int tmp;
+        if(a>b){
+            tmp1=a;
+            a=b;
+            b=tmp1;
+        }
+        else if(b>c){
+            tmp2=b;
+            b=c;
+            c=tmp3;
+        }
+        else if(c>d){
+            tmp3=c;
+            c=d;
+            d=tmp3;
+    }
+}
