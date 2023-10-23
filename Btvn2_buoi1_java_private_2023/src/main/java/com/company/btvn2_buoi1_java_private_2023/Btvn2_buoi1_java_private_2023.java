c82ffb60d99dc1d6a9af5b12fbfab313faeec4b8
Tung29022004@ <tungpv2902@gmail.com>
2023-10-23 19:31:07 +0700

    Bai_2

--- Btvn2_buoi1_java_private_2023/src/main/java/com/company/btvn2_buoi1_java_private_2023/Btvn2_buoi1_java_private_2023.java
+++ Btvn2_buoi1_java_private_2023/src/main/java/com/company/btvn2_buoi1_java_private_2023/Btvn2_buoi1_java_private_2023.java
@@ -12,16 +12,20 @@ import java.util.Scanner;
 public class Btvn2_buoi1_java_private_2023 {
 
     public static void main(String[] args) {
-        int a, b,tmp=0;
-        String kq="";
+        int a, b, tmp=0;
+        String kq = "";
         Scanner s = new Scanner(System.in);
         a = s.nextInt();
         b = s.nextInt();
-        int i = 0;
+
         while (a > 0) {
             tmp = a % b;
             a = a / b;
-            kq=tmp+kq;
+            if (tmp >= 10 && tmp <= 15) {
+                kq = (char)('A' + (tmp - 10)) + kq;
+            } else {
+                kq = tmp + kq;
+            }
         }
         System.out.println(kq);
     }
---
+++ Btvn3_buoi1_java_private_2023/src/main/java/com/company/btvn3_buoi1_java_private_2023/Btvn3_buoi1_java_private_2023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.btvn3_buoi1_java_private_2023;
+
+/**
+ *
+ * @author 84393
+ */
+public class Btvn3_buoi1_java_private_2023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
